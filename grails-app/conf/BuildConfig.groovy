fe26f9ae22369fa1998a19f6af1e5165ac5dad90
Vitalii Samolovskikh <kefir@perm.ru>
2013-09-28 12:54:09 +0600

    Upgrade Grails version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,7 +52,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         //compile ":h2:0.2.6"
-        runtime ":hibernate:$grailsVersion"
+        runtime ":hibernate:3.6.10.1"
         //runtime ":jquery:1.8.3"
         //runtime ":resources:1.1.6"
 
@@ -61,7 +61,7 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
+        build ":tomcat:7.0.42"
 
         //runtime ":database-migration:1.3.2"
 
